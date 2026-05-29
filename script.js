ceb7e33cb7f797ab68d764e73b83ee653b622291
colectivamar <memoriarteresis@gmail.com>
2026-05-29 17:21:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sheetURL =
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1wk_XxOasJFf41t2DGelvTg8ouTB8S9Na2jPe5QAOlEM8ZOR7yTr8BH-J4pDxJZJMXmkDoYlLipql/pub?gid=0&single=true&output=csv";
   
 const archivoContainer = document.getElementById("archivo-vivo");
 const modal = document.getElementById("modal");
