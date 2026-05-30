2688ceccb88f38e2837a227ca53503a7291c28a2
colectivamar <memoriarteresis@gmail.com>
2026-05-29 19:28:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sheetURL =
-  "https://corsproxy.io/?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vS1wk_XxOasJFf41t2DGelvTg8ouTB8S9Na2jPe5QAOlEM8ZOR7yTr8BH-J4pDxJZJMXmkDoYlLipql/pub?gid=0%26single=true%26output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1wk_XxOasJFf41t2DGelvTg8ouTB8S9Na2jPe5QAOlEM8ZOR7yTr8BH-J4pDxJZJMXmkDoYlLipql/pubhtml?gid=0&single=true";
 const archivoContainer = document.getElementById("archivo-vivo");
 const modal = document.getElementById("modal");
 const modalContent = document.getElementById("modal-content");
